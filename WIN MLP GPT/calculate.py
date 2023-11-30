e36f79a3193fd41be153f5434a28949c39a516ee
Kai Huang <kaihuang1122@gmail.com>
2023-11-30 14:06:59 +0800

    win

---
+++ WIN MLP GPT/calculate.py
@@ -0,0 +1,13 @@
+#! /Library/Frameworks/Python.framework/Versions/3.11/bin/python3
+import csv
+import sys
+import os
+import numpy as np
+from tqdm import tqdm
+out = []
+for x in (open("G:\ML\ML-final\/html.2023.final.data/sno_test_set.txt").readlines()):
+    fh = open(f"G:\ML\ML-final\/MLP GPT/log/given_err200/{x[:-1]}.out")
+    table = fh.readlines()[-1].split(' ')[-1][:-1]
+    out.append(np.float64(table))
+
+print(np.mean(out))
---
+++ WIN MLP GPT/output/predict/main.py
@@ -0,0 +1,10 @@
+import csv
+with open("G:\ML\ML-final\MLP GPT\output\predict\submission.csv") as fh:
+    table = list(csv.reader(fh))
+fout = open("G:\ML\ML-final\MLP GPT\output\predict\submit.csv", "w")
+writter = csv.writer(fout)
+writter.writerow(table[0])
+
+for i in range(112):#times 5761
+    for j in range(1, 5761, 20):
+        writter.writerow(table[i*5761+j])
